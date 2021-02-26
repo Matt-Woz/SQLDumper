28231fd1e5ac72a8608fa0abdc25c2d03356f40f
wozakows <matt.wozakowski@gmail.com>
2021-02-26 13:21:45 +0000

    fixed .sh again

--- runprogram.sh
+++ runprogram.sh
@@ -1,7 +1,6 @@
-#!/bin/bash	
-echo "Welcome - Compiling code"	
-rm ./*.class	
-javac -cp "../sqlite-jdbc-3.7.2.jar:." ./Coursework.java DatabaseDumper.java DatabaseDumper201.java	
-echo "Running.."	
-ls	
-java -cp "../sqlite-jdbc-3.7.2.jar:." Coursework "$1" > "$2" 
+#!/bin/bash
+read -p "Enter name of database to dump with .db: " database
+read -p "Enter name of sql file to store with .sql: " statement
+javac *.java
+java -cp "sqlite-jdbc-3.7.2.jar:." Coursework $database $statement>$statement
+echo "finished"
