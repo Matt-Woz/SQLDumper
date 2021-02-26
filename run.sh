de658d8e5791cd4d0787e1ac100be36cff273512
Matt-Woz <matt.wozakowski@gmail.com>
2021-02-26 03:34:12 +0000

    added bash file to run program

---
+++ run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "Welcome - Compiling code"
+rm ./*.class
+javac -cp "../sqlite-jdbc-3.7.2.jar:." ./Coursework.java DatabaseDumper.java DatabaseDumper201.java
+echo "Running.."
+ls
+java -cp "../sqlite-jdbc-3.7.2.jar:." Coursework "$1" > "$2"
